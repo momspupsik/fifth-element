cc136a7e50848476e330d61f71e68cf05259226e
daddy <viktorkadrov@gmail.com>
2023-09-15 06:18:51 +0300

    fix(dev): fix button

--- src/web-ui/main/urls.py
+++ src/web-ui/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('plk', views.plk),
+    path('upload', views.upload)
 ]
--- src/web-ui/main/views.py
+++ src/web-ui/main/views.py
@@ -7,3 +7,14 @@ def index(request):
 def plk(request):
     return render(request,'main/plk.html')
 
+def upload(request):
+    for key, file in request.FILES.items():
+        path = "../dcn/orchestrator/scripts/" + file.name
+        dest = open(path, 'wb')
+        if file.multiple_chunks:
+            for c in file.chunks():
+                dest.write(c)
+        else:
+            dest.write(file.read())
+        dest.close()
+    return render(request,'main/plk.html')
